4d1fa2c14ff100b160d3a6dc88346977811a70dd
YuJieZhang <19939926438@163.com>
2023-09-02 20:36:23 +0800

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+import { pwa } from './config/pwa'
+
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+    '@vite-pwa/nuxt',
+  ],
+
+  experimental: {
+    payloadExtraction: false,
+    inlineSSRStyles: false,
+    renderJsonPayloads: true,
+    typedPages: true,
+  },
+
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
+
+  colorMode: {
+    classSuffix: '',
+  },
+
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
+      },
+    },
+    prerender: {
+      crawlLinks: false,
+      routes: ['/'],
+      ignore: ['/hi'],
+    },
+  },
+
+  pwa,
+
+  devtools: {
+    enabled: true,
+  },
+})
