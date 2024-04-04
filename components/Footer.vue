84092e6b1f239303f6b7ea27e33b8b0712283e6d
zyhnbyyds <19939926438@163.com>
2024-04-04 21:47:01 +0800

    fix: link set err

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,8 +5,8 @@ const app = useAppConfig()
 <template>
   <div text="xl gray4" border-t="1px solid gray-400 op30" mx-5 h-65px flex justify-center p-5>
     <div flex="~ gap1 col-center" transition-all hover="op40">
-      <img h-4 w-4 src="https://beian.miit.gov.cn/img/bg_logo.2ce2f33d.png" alt="工信部" srcset="">
-      <NuxtLink external text-common target="_blank" to="https://mydailylife.oss-cn-shanghai.aliyuncs.com/ipc-logo.png">
+      <img h-4 w-4 src="https://mydailylife.oss-cn-shanghai.aliyuncs.com/ipc-logo.png" alt="工信部" srcset="">
+      <NuxtLink external text-common target="_blank" to="https://beian.miit.gov.cn/">
         {{ app.icp }}
       </NuxtLink>
     </div>
